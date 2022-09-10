0e3f7a0a8e0bd85e02f7ef3bde9362d35f9e35b9
yxxiin <12203369@inha.edu>
2022-09-10 22:41:48 +0900

    using Scanner, string to integer

--- src/Sum.java
+++ src/Sum.java
@@ -5,11 +5,13 @@ public class Sum {
         System.out.println("두 수를 입력하세요.");
 
         Scanner scanner = new Scanner(System.in);
-        String inputValue = scanner.nextLine();
+        String s1, s2;
 
-        String[] splitedValue = inputValue.split(" ");
-        int first = Integer.parseInt(splitedValue[0]);
-        int second = Integer.parseInt(splitedValue[1]);
+        s1 = scanner.next();
+        s2 = scanner.next();
+
+        int first = Integer.parseInt(s1);
+        int second = Integer.parseInt(s2);
 
         int result = first + second;
         System.out.println("두 수의 합은 " + result + "입니다.");
