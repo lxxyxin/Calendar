fa6aabd246e5001ef57595a4dc6b56e14b944b9e
yxxiin <12203369@inha.edu>
2022-09-14 18:02:42 +0900

    using if-else (+not using array)

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,10 +1,49 @@
+import java.util.Scanner;
 public class Calendar {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(" 일 월 화 수 목 금 토");
         System.out.println("-------------------");
         System.out.println(" 1  2  3  4  5  6  7");
         System.out.println(" 8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+
+
+        //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+        System.out.println("달을 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        int month = Integer.parseInt(a);
+        int day;
+        if (month >0 && month <13) {
+            if (month == 1) {
+                day = 31;
+            } else if (month == 2) {
+                day = 28;
+            } else if (month == 3) {
+                day = 31;
+            } else if (month == 4) {
+                day = 30;
+            } else if (month == 5) {
+                day = 31;
+            } else if (month == 6) {
+                day = 30;
+            } else if (month == 7) {
+                day = 31;
+            } else if (month == 8) {
+                day = 31;
+            } else if (month == 9) {
+                day = 30;
+            } else if (month == 10) {
+                day = 31;
+            } else if (month == 11) {
+                day = 30;
+            } else {
+                day = 31;
+            }
+            System.out.println(month + "월은 " +day + "일까지 있습니다.");
+        } else {
+            System.out.println("ㅌㅌ");
+        }
     }
 }
