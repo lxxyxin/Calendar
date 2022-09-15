b42dc55c273ed6f5b516a06d2c01faa7e35b3bd1
yxxiin <12203369@inha.edu>
2022-09-15 15:00:51 +0900

    print calendar(1) - my solution

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,31 +1,54 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Calendar {
-    private final int[] MaxDaysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
-    public int GetMaxDaysOfMonth(int month){
-        return MaxDaysOfMonth[month -1];
+    //private final int[] MaxDaysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
+    //public int GetMaxDaysOfMonth(int month){
+      //  return MaxDaysOfMonth[month -1];
+    //}
+    public void GetCalendar(int month) {
+        System.out.println("일 월 화 수 목 금 토");
+        System.out.println("------------------");
+
+        if (month == 2) {
+            System.out.println("1  2  3  4  5  6  7");
+            System.out.println("8  9 10 11 12 13 14");
+            System.out.println("15 16 17 18 19 20 21");
+            System.out.println("22 23 24 25 26 27 28");
+        } else if (month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        {
+            System.out.println("1  2  3  4  5  6  7");
+            System.out.println("8  9 10 11 12 13 14");
+            System.out.println("15 16 17 18 19 20 21");
+            System.out.println("22 23 24 25 26 27 28");
+            System.out.println("29 30 31");
+        } else {
+            System.out.println("1  2  3  4  5  6  7");
+            System.out.println("8  9 10 11 12 13 14");
+            System.out.println("15 16 17 18 19 20 21");
+            System.out.println("22 23 24 25 26 27 28");
+            System.out.println("29 30");
+        }
     }
+
     public static void main(String[] args) {
 
-        //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+        //숫자를 입력받아 해당하는 달의 달력 출력
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
         String PROMPT = "CAL> ";
 
-        int month = 1;
         while (true) {
             System.out.println("월을 입력하세요.");
             System.out.print(PROMPT);
-            month = scanner.nextInt();
+            int month = scanner.nextInt();
             if (month == -1){
                 break;
             }
             if (month >12){
                 continue;
             }
-            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.GetMaxDaysOfMonth(month));
+            cal.GetCalendar(month);
         }
-        System.out.println("Have a nice day!");
+        System.out.println("Bye~");
         scanner.close();
     }
 }
