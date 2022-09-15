43d102fcff2523a368805a5d1552ed0b845c36ea
yxxiin <12203369@inha.edu>
2022-09-15 17:01:29 +0900

    print calendar(2) - leap year(correction)

--- src/Prompt.java
+++ src/Prompt.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Prompt {
 
-    private final static String PROMPT ="cal> ";
-
     public void runPrompt(){
         //숫자를 입력받아 해당하는 달의 달력 출력
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
-        int month = 1;
 
         while (true) {
-            System.out.println("월을 입력하세요.");
-            System.out.print(PROMPT);
+            System.out.println("연도을 입력하세요.");
+            System.out.print("YEAR> ");
             int year = scanner.nextInt();
-            month = scanner.nextInt();
+            System.out.println("달을 입력하세요.");
+            System.out.print("MONTH> ");
+            int month = scanner.nextInt();
 
             if (month == -1){
                 break;
