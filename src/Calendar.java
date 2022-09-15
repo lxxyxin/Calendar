4f6dc04a29fb8f7be2be1462de5b194e57c24a50
yxxiin <12203369@inha.edu>
2022-09-15 14:21:36 +0900

    using while loop with exit condition

--- src/Calendar.java
+++ src/Calendar.java
@@ -13,10 +13,16 @@ public class Calendar {
         String PROMPT = "CAL> ";
 
         int month = 1;
-        while (month != -1) {
+        while (true) {
             System.out.println("월을 입력하세요.");
             System.out.print(PROMPT);
             month = scanner.nextInt();
+            if (month == -1){
+                break;
+            }
+            if (month >12){
+                continue;
+            }
             System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.GetMaxDaysOfMonth(month));
         }
         System.out.println("Have a nice day!");
