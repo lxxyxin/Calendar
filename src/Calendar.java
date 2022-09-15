e56feed3e4e7fa7551d6e1c6f9e26e02bb5cfe2c
yxxiin <12203369@inha.edu>
2022-09-15 14:12:06 +0900

    using while loop with exit condition

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Calendar {
     private final int[] MaxDaysOfMonth = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -5,25 +6,20 @@ public class Calendar {
         return MaxDaysOfMonth[month -1];
     }
     public static void main(String[] args) {
-        System.out.println(" 일 월 화 수 목 금 토");
-        System.out.println("-------------------");
-        System.out.println(" 1  2  3  4  5  6  7");
-        System.out.println(" 8  9 10 11 12 13 14");
-        System.out.println("15 16 17 18 19 20 21");
-        System.out.println("22 23 24 25 26 27 28");
-
 
         //숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-        System.out.println("달을 입력하세요.");
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
-        System.out.println("반복횟수를 입력하세요.");
-        int num = scanner.nextInt();
-        System.out.println("달을 입력하세요.");
-        for (int i=0;i<num;i++) {
-            int month = scanner.nextInt();
+        String PROMPT = "CAL> ";
+
+        int month = 1;
+        while (month != -1) {
+            System.out.println("월을 입력하세요.");
+            System.out.print(PROMPT);
+            month = scanner.nextInt();
             System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.GetMaxDaysOfMonth(month));
         }
+        System.out.println("Have a nice day!");
         scanner.close();
     }
 }
